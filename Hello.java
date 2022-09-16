f4ade85132dcdc87e767a7e561cbeb6613959501
Гусева Полина Николаевна <pn_guseva@mail.ru>
2022-09-16 13:26:58 +0300

    Add 2 in the end

--- Hello.java
+++ Hello.java
@@ -1,8 +1,8 @@
 class Main
 {
 	public static void main(String[] args) {
-        System.out.println("A");
-		System.out.println("Hello World1");
+        	System.out.println("A");
+		System.out.println("Hello, world!12");
 		System.out.println("B");
 	}
 }
